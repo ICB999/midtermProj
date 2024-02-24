5bae5ee7f30f76c58fbc9434f7d2367ff0d6f311
ICB999 <bensey873@gmail.com>
2024-02-24 10:29:57 +0700

    Revert "Revert "Revert "Added Auth and login"""
    This reverts commit a6d04c37093b01b13c10cb6eef02c53e3b8b95fb.

--- app/src/main/java/com/example/productapp/MainActivity.java
+++ app/src/main/java/com/example/productapp/MainActivity.java
@@ -2,35 +2,13 @@ package com.example.productapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
-    FirebaseUser user;
-    TextView email;
-    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        user = mAuth.getCurrentUser();
-        email = findViewById(R.id.userEmail);
-        email.setText(String.valueOf(user.getEmail()));
-        button = findViewById(R.id.signOut);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
     }
 }
